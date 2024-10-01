80c45ef3c9e1e71f84cb79d98d7cae7ada3613d7
Davidcasstella <brayan.lopez07@uptc.edu.co>
2024-09-30 23:02:06 -0500

    pa la primera rama

--- Jun.py
+++ Jun.py
@@ -1,3 +1,5 @@
 print("bueno quisiera que funiones la rama principal na mas ")
 print(" actualizando la rama principal")
 print("la actualzaice una vez mas ")
+
+print("esto no deberia quedar en la rama principal")
