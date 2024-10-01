765d97f47adde188283ca7fb367cef25e318a1c1
Davidcasstella <brayan.lopez07@uptc.edu.co>
2024-09-30 23:15:33 -0500

    se agrega hasta que quede bien

--- Jun.py
+++ Jun.py
@@ -3,3 +3,5 @@ print(" actualizando la rama principal")
 print("la actualzaice una vez mas ")
 
 print("esto no deberia quedar en la rama principalj")
+
+print("aqui se clona el trabajo pero la rama princial no o si?")
