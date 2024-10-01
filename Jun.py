1c9a46b0e773d6f0942e09ec2cf6a994c9ab0477
Davidcasstella <brayan.lopez07@uptc.edu.co>
2024-09-30 23:08:04 -0500

    para que se agregue a mi nueva rama

--- Jun.py
+++ Jun.py
@@ -2,4 +2,4 @@ print("bueno quisiera que funiones la rama principal na mas ")
 print(" actualizando la rama principal")
 print("la actualzaice una vez mas ")
 
-print("esto no deberia quedar en la rama principal")
+print("esto no deberia quedar en la rama principalj")
